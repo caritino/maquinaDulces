4e68cbe0d605d9efbb3e89e9726aa6e5c46c6e73
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-06-23 03:13:47 -0500

    se agregaron 4 leds, y tres botones

--- maquinaDulces.ino
+++ maquinaDulces.ino
@@ -1,16 +1,28 @@
-#include <Display.h>
-int rojo =13,verde=12,amarillo=11;
-int boton1 = 7, boton2=6, boton3=5;
+int rojo = 13;
+int verde = 12;
+int amarillo = 11;
+int azul = 10;
 
-Display dis(26,28,30,32,34,36,38);
+int boton1 = 7;
+int boton2 = 6;
+int boton3 = 5;
+
 int i=0;
 
+//  Chicles $2
+//  Galletas $3
+//  Chocolate $4
+//  Papas $5
+//  Refresco $8
 
 void setup() {
   Serial.begin(9600);
+
   pinMode(rojo, OUTPUT);
   pinMode(verde, OUTPUT);
   pinMode(amarillo, OUTPUT);
+  pinMode(azul, OUTPUT);
+
   pinMode(boton1, INPUT);
   pinMode(boton2, INPUT);
   pinMode(boton3, INPUT);
@@ -18,41 +30,56 @@ void setup() {
 
 void loop() {
    
-int lec = analogRead(A0);
+  int lec = analogRead(A0);
 
-int eboton1=digitalRead(boton1);
-int boton2=digitalRead(boton2);
-int boton3=digitalRead(boton3);
+  int estadoBoton1=digitalRead(boton1);
+  int estadoBoton2=digitalRead(boton2);
+  int estadoBoton3=digitalRead(boton3);
 
     
-    if(lec<=512){
+  if(lec<=512){
       delay(500);
       i++;
-    
-      }
+  }
 
-  if(i==2){
+  if(i==2){                       //  Chicles $2 
     digitalWrite(rojo, HIGH);
     digitalWrite(verde, LOW);
     digitalWrite(amarillo, LOW);
-  }else if(i>2 && i==4){
+    digitalWrite(azul, LOW);
+  }else if(i==3){                 //  Galletas $3
     digitalWrite(rojo, HIGH);
     digitalWrite(verde, HIGH);
     digitalWrite(amarillo, LOW);
-  }else if(i>4 && i==6){
+    digitalWrite(azul, LOW);
+  }else if(i==4){                 //  Chocolate $4
     digitalWrite(rojo, HIGH);
     digitalWrite(verde, HIGH);
     digitalWrite(amarillo, HIGH);
-  }else if(i<2){
+    digitalWrite(azul, LOW);
+  }else if(i==5){                 //  Papas $5
+    digitalWrite(rojo, HIGH);
+    digitalWrite(verde, HIGH);
+    digitalWrite(amarillo, HIGH);
+    digitalWrite(azul, HIGH);
+  }else if(i<2){                  //  Apagar todo       
     digitalWrite(rojo, LOW);
     digitalWrite(verde, LOW);
     digitalWrite(amarillo, LOW);
+    digitalWrite(azul, LOW);
   }
 
-  if(eboton1 == HIGH){
+  if(estadoBoton1 == HIGH){
     delay(1000);
     i=i-2;
-  }else if(
+  }else if(estadoBoton2 == HIGH){
+    delay(1000);
+    i=i-3;
+  }else if(estadoBoton3 == HIGH){
+    delay(1000);
+    i=i-4;
+  }
+
     
  Serial.println (i);
  
